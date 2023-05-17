b60c9bb21368b41031aa448f84add12fcdf61c56
Erick Villalobos <villalobose143@gmail.com>
2023-05-17 14:51:16 -0500

    edit title

--- src/pages/TasksPage.jsx
+++ src/pages/TasksPage.jsx
@@ -16,7 +16,7 @@ const TasksPage = () => {
   }
 
   return (
-    <div className="pt-20">
+    <div className="pt-20 flex flex-col gap-4">
       <h1 className="text-5xl text-white font-bold text-center">Clients</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-2">{renderMain()}</div>
     </div>
